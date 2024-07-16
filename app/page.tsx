1ff1d6e5825d97fd1f2696c247205784b34226e3
sam-christopher07 <samchristophergit07@gmail.com>
2024-07-17 00:28:01 +0530

    application build sucessfully

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === "true";
 
-export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP Verification | PasskeyModal */}
+      {isAdmin && <PasskeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -22,18 +24,18 @@ export default function Home() {
             <p className="justify-items-end text-dark-600 xl:text-left">
               © 2024 Health Align
             </p>
-            <Link href="/?admin=true"  className="text-green-500">
-            Admin
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
             </Link>
           </div>
         </div>
       </section>
       <Image
-      src="/assets/images/onboarding-img1.jpg"
-      height={1000}
-      width={1000}
-      alt="patient"
-      className="side-img max-w-[50%]"
+        src="/assets/images/onboarding-img1.jpg"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
       />
     </div>
   );
--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -43,6 +43,19 @@ export const getUser = async (userId: string) => {
   }
 };
 
+export const getPatient = async (userId: string) => {
+  try {
+    const patients = await databases.listDocuments(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      [Query.equal("userId", userId)]
+    );
+    return parseStringify(patients.documents[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const registerPatient = async ({
   identificationDocument,
   ...patient
@@ -61,6 +74,7 @@ export const registerPatient = async ({
 
       file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
     }
+
 
     const newPatient = await databases.createDocument(
       DATABASE_ID!,
